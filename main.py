1dc4f57787e0748107ae98d041e856eafe8cf7ad
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-04-16 11:01:51 +0200

    Dunno it does something. Some checking and stuff and printing.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import requests
 
 
+def check_input(number):
+    try:
+        val = int(str(number))
+        return val
+    except ValueError:
+        print("That's not an int!")
+
+
 print("Input the username you wish to see the activity of")
 username = input()
 print("Requested data of: "+username)
+
+print("How much  recent activity do you wish to see?")
+number = input()
+number = check_input(number)
 
 
 def request_response(username):
@@ -16,14 +28,21 @@ def check_status(response):
     else:
         return f"NOT GOOD AAAA!!!! Error: {response.status_code}"
 
-def fetch_data(username):
-
-
-
-    pass
+def fetch_data(username,number,response):
+    data = response.json()
+    if not data:
+        print("No recent activity found.")
+        return
+    print("\nRecent Public Events:\n")
+    for event in data[:number]:  # Just show the 5 most recent
+        print(f"- Type: {event['type']}")
+        print(f"  Repo: {event['repo']['name']}")
+        print(f"  Created at: {event['created_at']}\n")
 
 
 x = request_response(username)
-print(check_status(x))
+if check_status(x) == "Check OK":
+    fetch_data(username,number,x)
+else:
+    raise Exception("Something went wrong")
 
-print(x.json())
